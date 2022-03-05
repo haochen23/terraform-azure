833a18f2fb8793378b776213427e3953b4435702
Hao Chen <hao.chen23@outlook.com>
2022-03-05 20:56:10 +1300

    add aks IaC code

---
+++ aks/main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.96.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = "eastus"
+  tags = {
+    Environment = var.env_tag
+  }
+}
+
+# # file share
+# resource "azurerm_storage_account" "storageaccount" {
+#   name                     = "aksefkstorageaccount"
+#   resource_group_name      = azurerm_resource_group.rg.name
+#   location                 = azurerm_resource_group.rg.location
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+# }
+
+# resource "azurerm_storage_share" "fileshare" {
+#   name                 = "aksefkfileshare"
+#   storage_account_name = azurerm_storage_account.storageaccount.name
+#   quota                = 50
+# }
+
+
+# aks
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "aksefkerictest"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = var.env_tag
+  }
+}
---
+++ aks/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  default = "aksefkericrg"
+}
+
+variable "aks_name" {
+  default = "aksefktsteric"
+}
+
+variable "env_tag" {
+  default = "test"
+}
