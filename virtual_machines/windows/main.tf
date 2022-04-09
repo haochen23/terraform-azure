d46c2c56189ad7b32c0b0a99f6b004f20b180552
Hao Chen <hao.chen23@outlook.com>
2022-04-09 15:37:47 +1200

    add a windows vm

---
+++ virtual_machines/windows/main.tf
@@ -0,0 +1,114 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.1.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+locals {
+  resource_group="app-grp"
+  location="East US"
+}
+
+data "azurerm_subnet" "SubnetA" {
+  name                 = "SubnetA"
+  virtual_network_name = "app-network"
+  resource_group_name  = local.resource_group
+}
+
+resource "azurerm_resource_group" "app_grp"{
+  name=local.resource_group
+  location=local.location
+}
+
+resource "azurerm_virtual_network" "app_network" {
+  name                = "app-network"
+  location            = local.location
+  resource_group_name = azurerm_resource_group.app_grp.name
+  address_space       = ["10.0.0.0/16"]
+
+  subnet {
+    name           = "SubnetA"
+    address_prefix = "10.0.1.0/24"
+  }  
+}
+
+resource "azurerm_network_interface" "app_interface" {
+  name                = "app-interface"
+  location            = local.location
+  resource_group_name = local.resource_group
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = data.azurerm_subnet.SubnetA.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.app_public_ip.id
+  }
+
+  depends_on = [
+    azurerm_virtual_network.app_network,
+    azurerm_public_ip.app_public_ip
+  ]
+}
+
+resource "azurerm_windows_virtual_machine" "app_vm" {
+  name                = "appvm"
+  resource_group_name = local.resource_group
+  location            = local.location
+  size                = "Standard_D2s_v3"
+  admin_username      = "demousr"
+  admin_password      = "Azure@123"
+  network_interface_ids = [
+    azurerm_network_interface.app_interface.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+
+  depends_on = [
+    azurerm_network_interface.app_interface
+  ]
+}
+
+resource "azurerm_public_ip" "app_public_ip" {
+  name                = "app-public-ip"
+  resource_group_name = local.resource_group
+  location            = local.location
+  allocation_method   = "Static"
+}
+
+resource "azurerm_managed_disk" "data_disk" {
+  name                 = "data-disk"
+  location             = local.location
+  resource_group_name  = local.resource_group
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = 16
+}
+# Then we need to attach the data disk to the Azure virtual machine
+resource "azurerm_virtual_machine_data_disk_attachment" "disk_attach" {
+  managed_disk_id    = azurerm_managed_disk.data_disk.id
+  virtual_machine_id = azurerm_windows_virtual_machine.app_vm.id
+  lun                = "0"
+  caching            = "ReadWrite"
+  depends_on = [
+    azurerm_windows_virtual_machine.app_vm,
+    azurerm_managed_disk.data_disk
+  ]
+}
