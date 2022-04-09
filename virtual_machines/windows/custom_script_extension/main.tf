e55bf8b366913e957ac998689d1e28c06d41f0f8
Hao Chen <hao.chen23@outlook.com>
2022-04-09 16:40:42 +1200

    add nsg

--- virtual_machines/windows/custom_script_extension/main.tf
+++ virtual_machines/windows/custom_script_extension/main.tf
@@ -182,3 +182,30 @@ resource "azurerm_virtual_machine_extension" "vm_extension" {
 SETTINGS
 
 }
+
+resource "azurerm_network_security_group" "app_nsg" {
+  name                = "app-nsg"
+  location            = local.location
+  resource_group_name = local.resource_group
+
+# We are creating a rule to allow traffic on port 80
+  security_rule {
+    name                       = "Allow_HTTP"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg_association" {
+  subnet_id                 = azurerm_subnet.SubnetA.id
+  network_security_group_id = azurerm_network_security_group.app_nsg.id
+  depends_on = [
+    azurerm_network_security_group.app_nsg
+  ]
+}
