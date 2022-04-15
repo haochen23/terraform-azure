467fd8e3ec89760df3b54c8ba2b6cec3ea735ffd
Hao Chen <hao.chen23@outlook.com>
2022-04-15 17:05:27 +1200

    Add azure sql example

---
+++ databases/azure-sql-database/main.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.1.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+locals {
+  resource_group="app-grp"
+  location="West US"
+}
+
+
+resource "azurerm_resource_group" "app_grp"{
+  name=local.resource_group
+  location=local.location
+}
+
+resource "azurerm_mssql_server" "app_server" {
+  name                         = "appsqlsvr231"
+  resource_group_name          = azurerm_resource_group.app_grp.name
+  location                     = local.location
+  version             = "12.0"
+  administrator_login          = "sqladmin"
+  administrator_login_password = "Azure@123"
+}
+
+resource "azurerm_mssql_database" "app_db" {
+  name                = "appdb"
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 4
+  read_scale     = true
+  sku_name       = "BC_Gen5_2"
+  zone_redundant = false
+  server_id         = azurerm_mssql_server.app_server.id
+   depends_on = [
+     azurerm_mssql_server.app_server
+   ]
+}
+
+resource "azurerm_mssql_firewall_rule" "app_server_firewall_rule" {
+  name                = "app-server-firewall-rule"
+  server_id        = azurerm_mssql_server.app_server.id
+  start_ip_address    = "101.98.111.142"
+  end_ip_address      = "101.98.111.142"
+  depends_on = [
+    azurerm_mssql_server.app_server
+  ]
+}
